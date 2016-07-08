b9d748108c31be30c666fc5df34a6dfda1f34b9b
森田雄太郎 <yu8ta8ro@icloud.com>
2016-07-08 19:57:34 +0900

    Added function of new&create at baba_controller

--- app/controllers/babas_controller.rb
+++ app/controllers/babas_controller.rb
@@ -2,4 +2,18 @@ class BabasController < ApplicationController
   def index
     @babas = Baba.order('id DESC').page(params[:page]).per(6)
   end
+
+  def new
+    @baba = Baba.new
+  end
+
+  def create
+    Baba.create(create_params)
+    redirect_to action: :index
+  end
+
+  private
+  def create_params
+    params.require(:baba).permit(:title, :concept)
+  end
 end
