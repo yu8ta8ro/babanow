7bc30f643eba69636bcea36e4a17f4695d2ed92f
森田雄太郎 <yu8ta8ro@icloud.com>
2016-07-08 19:39:21 +0900

    Edified babas/index

--- app/controllers/babas_controller.rb
+++ app/controllers/babas_controller.rb
@@ -1,5 +1,5 @@
 class BabasController < ApplicationController
   def index
-    @babas = Baba.order('id DESC').page(params[:page]).per(5)
+    @babas = Baba.order('id DESC').page(params[:page]).per(6)
   end
 end
